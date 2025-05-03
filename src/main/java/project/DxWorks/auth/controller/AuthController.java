13f09bd1ac4bfb0e443fe2e8b6746ea9a0ace072
한재준 <gkswowns25@naver.com>
2025-05-04 00:14:03 +0900

    feat: join kakao Controller

--- src/main/java/project/DxWorks/auth/controller/AuthController.java
+++ src/main/java/project/DxWorks/auth/controller/AuthController.java
@@ -2,14 +2,26 @@ package project.DxWorks.auth.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.DxWorks.auth.dto.KakaoAuthRequestDto;
+import project.DxWorks.auth.dto.UserAccessTokenResponseDto;
 import project.DxWorks.auth.service.KakaoAuthService;
+import project.DxWorks.common.ui.Response;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final KakaoAuthService kakaoAuthService;
+
+    @PostMapping("/kakao")
+    public Response<UserAccessTokenResponseDto> authenticateWithKakao(@RequestBody KakaoAuthRequestDto requestDto){
+        UserAccessTokenResponseDto response = kakaoAuthService.processKakaoLogin(requestDto.accessToken());
+        return Response.ok(response);
+    }
 
 }
