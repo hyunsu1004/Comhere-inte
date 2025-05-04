14fce3f709be5d422ff1d4854f9f82ee97c04dfa
junjaebrother <86496610+junjaebrother@users.noreply.github.com>
2025-05-04 19:21:40 +0900

    Revert "Feat/community"

--- src/main/java/project/DxWorks/GeminiAI/repository/InbodyRepository.java
+++
@@ -1,8 +0,0 @@
-package project.DxWorks.GeminiAI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.DxWorks.GeminiAI.entity.Inbody;
-
-public interface InbodyRepository extends JpaRepository<Inbody, Long> {
-
-}
--- src/main/java/project/DxWorks/goal/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package project.DxWorks.goal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project.DxWorks.goal.entity.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
