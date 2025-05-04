0115ec2ab9ad8d81a73783ff9784921733eb31a0
chominju <cmj021002@knu.ac.kr>
2025-05-04 19:12:02 +0900

    feat: Community entity 구현

--- src/main/java/project/DxWorks/community/entity/Community.java
+++ src/main/java/project/DxWorks/community/entity/Community.java
@@ -3,6 +3,10 @@ package project.DxWorks.community.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import project.DxWorks.post.entity.Post;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,11 +16,23 @@ public class Community {
     @Id
     @GeneratedValue
     @Column(name = "community_id")
-    private Long communityId;
+    private Long id;
 
     @Enumerated(EnumType.STRING)
     private CommunityCategory communityCategory;
 
+    @OneToMany(mappedBy = "community", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
 
+
+    public void addPost(Post post) {
+        posts.add(post);
+        post.setCommunity(this);
+    }
+
+    public void removePost(Post post) {
+        posts.remove(post);
+        post.setCommunity(null);
+    }
 }
 
